8e2038e23dc55c93c53e58031e484b937ea4d52f
Alexander Tisko <alexandertisko@hotmail.com>
2022-11-09 12:28:56 -0300

    Initial Configuration

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+// Calling controllers
+var indexController = require('../controllers/index')
+
+// Defining routings
+router.get('/', indexController.index)
+
+module.exports = router;
