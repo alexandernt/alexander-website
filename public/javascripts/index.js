dd9dc6bf6b06ac5ef3eacc937f038443d8b7ddb0
Alexander Tisko <alexandertisko@hotmail.com>
2022-11-12 23:11:47 -0300

    Html and Style - Menu need to work

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,7 +3,12 @@ const box1 = document.querySelector('.box1')
 const box2 = document.querySelector('.box2')
 const box3 = document.querySelector('.box3')
 const navContainer = document.querySelector('.nav__container')
+const skillsContent = document.getElementsByClassName("skills__content");
+const skillsHeader = document.querySelectorAll('.skills__header');
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
 const html = document.documentElement
+const main = document.querySelectorAll('.main')
 var width = screen.width
 var height = screen.height
 
@@ -44,4 +49,34 @@ function removeMenu () {
 
 navIcon.addEventListener('click', function() {
     doAction()
+})
+
+/*-------------------------------Skills---------------------------------*/
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+    console.log(this.parentNode.className)
+    for (i = 0; i < skillsContent.length; i++) {
+        if (itemClass === 'skills__content skills__close') this.parentNode.className = 'skills__content skills__open' 
+        if (itemClass === 'skills__content skills__open') this.parentNode.className = 'skills__content skills__close'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+});
+
+/*-------------------------------Qualifications---------------------------------*/
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
 })
