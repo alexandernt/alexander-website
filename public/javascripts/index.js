16806c098fbbe939e3d2cff4c78b43861cde2548
Alexander Tisko <alexandertisko@hotmail.com>
2022-11-13 15:41:49 -0300

    Html and Style - FAQs, Contact, CV, Education and Skills

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,28 +1,19 @@
+const html = document.documentElement
+const main = document.querySelectorAll('.main')
+var width = screen.width
+var height = screen.height
+
+/*------------------------------- Menu ---------------------------------*/
+
+// Variables
 const navIcon = document.querySelector('.nav__icon')
 const box1 = document.querySelector('.box1')
 const box2 = document.querySelector('.box2')
 const box3 = document.querySelector('.box3')
 const navContainer = document.querySelector('.nav__container')
 const skillsContent = document.getElementsByClassName("skills__content");
-const skillsHeader = document.querySelectorAll('.skills__header');
-const tabs = document.querySelectorAll('[data-target]');
-const tabContents = document.querySelectorAll('[data-content]');
-const html = document.documentElement
-const main = document.querySelectorAll('.main')
-var width = screen.width
-var height = screen.height
 
-// This function will count the user clicks on the hamburguer
-let clickCount = 1;
-function doAction () {
-    clickCount ++;
-    if ((clickCount % 2) === 0){
-        showMenu()
-    } else { 
-        removeMenu()
-    }
-}
-
+// Show Menu Function
 function showMenu () {
     navContainer.classList.add("nav__container--active")
     html.classList.add('overflowy__hidden')
@@ -35,6 +26,7 @@ function showMenu () {
     box1.classList.add('box-active')
 }
 
+// Remove Menu Function
 function removeMenu () {
     navContainer.classList.remove("nav__container--active")
     html.classList.remove('overflowy__hidden')
@@ -47,12 +39,28 @@ function removeMenu () {
     box1.classList.remove('box-active')
 }
 
+// This function will count the user clicks on the hamburguer and show or remove menu
+let clickCount = 1;
+function doAction () {
+    clickCount ++;
+    if ((clickCount % 2) === 0){
+        showMenu()
+    } else { 
+        removeMenu()
+    }
+}
+
+// Calling doAction function and showing or removing menu (final function)
 navIcon.addEventListener('click', function() {
     doAction()
 })
 
 /*-------------------------------Skills---------------------------------*/
 
+// Variables
+const skillsHeader = document.querySelectorAll('.skills__header');
+
+// Main Function
 function toggleSkills() {
     let itemClass = this.parentNode.className
     console.log(this.parentNode.className)
@@ -62,11 +70,17 @@ function toggleSkills() {
     }
 }
 
+// For each header, apply the toggleSkills function
 skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 });
 
 /*-------------------------------Qualifications---------------------------------*/
+// Variables
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
+
+// Main function
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.target)
@@ -80,3 +94,28 @@ tabs.forEach(tab => {
         tab.classList.add('qualification__active')
     })
 })
+
+/*-------------------------------FAQs---------------------------------*/
+
+// Variables
+const faqsTemplate = document.querySelectorAll('.faqs__template')
+const faqsHeader = document.querySelectorAll('.faqs__template--header')
+
+// Main Function
+function toggleFaqs() {
+    let itemClass = this.parentNode.className
+    for (i = 0; i < faqsTemplate.length; i++) {
+        if (itemClass === 'faqs__template faqs__template--closed') {
+            this.parentNode.className = 'faqs__template faqs__template--open'
+        }
+        if (itemClass === 'faqs__template faqs__template--open') {
+            this.parentNode.className = 'faqs__template faqs__template--closed'
+        }
+    }
+}
+
+// For each header called the toggleFaqs fucntion
+faqsHeader.forEach((el) => {
+    el.addEventListener('click', toggleFaqs)
+})
+
