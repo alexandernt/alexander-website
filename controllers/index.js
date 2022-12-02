520340a61b23fdc935e85988f6433ab80dc500ea
Alexander Tisko <alexandertisko@hotmail.com>
2022-12-02 11:31:01 -0300

    modifying single connection to pool connection

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,20 @@ let database = require('../database/db');
 
 exports.index = function (req, res, next) {
     var userCount = "SELECT COUNT(DISTINCT user_subscription_id) AS total_users FROM user_subscription";
-    database.connection.query(userCount, function(error, results, fields){
-      if(error) throw error
-      var count = results[0].total_users
-      res.render('index', {data: count})
-    });
+    database.pool.getConnection(function(err, connection){
+      if (err) throw err;
+      connection.query(userCount, function(error, results, feilds) {
+        connection.release();
+        if (error) throw error;
+        var count = results[0].total_users;
+        res.render('index', {data: count});
+      })
+    })
+    // database.connection.connect();
+    // database.connection.query(userCount, function(error, results, fields){
+    //   if(error) throw error
+    //   var count = results[0].total_users
+    //   res.render('index', {data: count})
+    // });
+    // database.connection.end();
 };
