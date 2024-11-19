328d86b9035bad1948d78672465351b6f78b0855
alexandernt <alexandertisko@hotmail.com>
2024-11-19 09:01:31 -0300

    refactor: integrated tailwind css

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("node_modules"));
 app.use(bodyParser.urlencoded({
   extended: true
 }))
